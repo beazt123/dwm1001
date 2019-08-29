80f70b9ebb1dc2bb3937286c78341eca3b99e39d
zhixuan <zhixuan.loh@reddotrobotics.com>
2019-08-29 11:51:10 +0800

    Change the package name to lowercase

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['DWM1001'],
+    packages=['dwm1001'],
     package_dir={'': 'src'})
 
 setup(**setup_args)
